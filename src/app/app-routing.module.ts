7578441be43f26749dccbae616bb98799901c093
Jakub Wasilewski <jakwasi01@gmail.com>
2023-12-09 18:02:57 +0100

    lab-d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TasksComponent} from "./tasks/tasks.component";
+import {ArchiveComponent} from "./archive/archive.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tasks', component: TasksComponent},
+  {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+  {path: 'archive', component: ArchiveComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
